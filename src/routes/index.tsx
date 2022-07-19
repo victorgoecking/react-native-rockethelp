0eaacda267c56a9f38ebfbaf6ec1d932c1c7c452
Victor Goecking <victorgoecking96@gmail.com>
2022-07-19 02:01:50 -0300

    :bug: Name function in index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,7 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { Signin } from '../screens/Signin';
 import { AppRoutes } from './app.routes';
 
-export function Route(){
+export function Routes(){
   return(
     <NavigationContainer>
       <AppRoutes />
